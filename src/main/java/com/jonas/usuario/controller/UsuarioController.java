3c45c73a27fae70f04ff96f71431bb95649ac970
Jonas <jonas.jgferro@gmail.com>
2025-08-20 23:50:35 -0300

    corrigindo ultimo commit erro dependências

--- src/main/java/com/jonas/usuario/controller/UsuarioController.java
+++ src/main/java/com/jonas/usuario/controller/UsuarioController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +16,7 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<UsuarioDTO> salvaUsuario(@ResponseBody UsuarioDTO usuarioDTO){
+    public ResponseEntity<UsuarioDTO> salvaUsuario(UsuarioDTO usuarioDTO){
     return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
     }
 }
--- src/main/java/com/jonas/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/jonas/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,7 @@
 package com.jonas.usuario.infrastructure.repository;
 
-import com.jonas.aprendendospring.infrastructure.entity.Endereco;
+
+import com.jonas.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
